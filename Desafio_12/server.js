2abf931352d0abe3c2b1f68576a246f460c324ed
Rafael <rrcamel@hotmail.com>
2022-12-01 18:15:34 -0300

    Desafio 12

---
+++ Desafio_12/server.js
@@ -0,0 +1,24 @@
+
+const app = require('./app') //express()
+// const PORT = process.env.PORT || 3000
+const PORT = process.argv[2] || 8000
+
+
+const initSocket = require('./utils/initSocket')
+
+const { Server: HttpServer } = require('http')
+const { Server: IOServer } = require('socket.io')
+
+const httpServer = new HttpServer(app)
+const io = new IOServer(httpServer)
+//------------------------------------------------------------------------
+initSocket(io)
+//--------------------------------------------------
+
+
+httpServer.listen(PORT, () => {
+    console.log(`SERVER listen on port ${PORT}`)
+})
+
+
+module.exports = app

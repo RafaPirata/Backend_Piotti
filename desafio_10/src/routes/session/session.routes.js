1830f7ad327c5fe6b2b4bc332c385f7ee4ed1ced
Rafael <rrcamel@hotmail.com>
2022-12-01 18:17:38 -0300

    Desafio 10

---
+++ desafio_10/src/routes/session/session.routes.js
@@ -0,0 +1,15 @@
+const { Router } = require('express')
+const { sessionGet, sessionLogout, sessionPostLogin } = require('../../controllers/session/session.controllers')
+const { authMiddleware } = require('../../middleware/auth.middleware')
+
+
+const routerSession = Router()
+
+
+
+
+routerSession.get('/', authMiddleware, sessionGet)
+routerSession.get('/logout', sessionLogout)
+routerSession.post('/login', sessionPostLogin )
+
+module.exports = routerSession
